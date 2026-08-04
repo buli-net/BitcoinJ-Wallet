873aeb45030ec7f33840b9e167656856769b7625
buli <hvlrcb@gmail.com>
2026-08-05 00:43:43 +0700

    Update Constants.java

--- app/src/main/java/com/example/thinkmobiles/bitcoinwalletsample/Constants.java
+++ app/src/main/java/com/example/thinkmobiles/bitcoinwalletsample/Constants.java
@@ -7,5 +7,5 @@ package com.example.thinkmobiles.bitcoinwalletsample;
 public class Constants {
     public static final String WALLET_NAME = "users_wallet";
 
-    public static boolean IS_PRODUCTION = false;
+    public static boolean IS_PRODUCTION = true;
 }
